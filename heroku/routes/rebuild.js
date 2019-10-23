f3cfa0f16eaaeca84f3f8a9faef89625ba2195f0
Stephan Smith <stephan@meenta.io>
2019-10-23 08:06:57 -0400

    Upated

--- heroku/routes/rebuild.js
+++ heroku/routes/rebuild.js
@@ -24,7 +24,43 @@ module.exports = (req, res) => {
 		${queryName} {
 			items {
 				_id
-				name
+				agents {
+					_id
+					address {
+						addressLine1
+						addressLine2
+						city
+						country
+						state
+						zipCode
+					}
+					descriptionSummary
+					email
+					logo {
+						_id
+						caption
+						credit
+						description
+						filename
+						mimeType
+						path
+						sourceUrl
+						title
+						uploadStatus
+					}
+					name
+					operators
+					phone
+					socialMedia {
+						facebook
+						instagram
+						twitter
+						youTube
+					}
+					type
+					websiteUrl
+					yearFounded
+				}
 				characteristics {
 					aged
 					covering
@@ -36,6 +72,120 @@ module.exports = (req, res) => {
 					texture
 				}
 				description
+				labelOrPhoto {
+					_id
+					caption
+					credit
+					description
+					filename
+					mimeType
+					path
+					sourceUrl
+					title
+					uploadStatus
+				}
+				name
+				pairings
+				photos {
+					photo {
+						_id
+						caption
+						credit
+						description
+						filename
+						mimeType
+						path
+						sourceUrl
+						title
+						uploadStatus
+					}
+				}
+				source
+				tastesLike {
+					_id
+					agents {
+						_id
+						address {
+							addressLine1
+							addressLine2
+							city
+							country
+							state
+							zipCode
+						}
+						descriptionSummary
+						email
+						name
+						operators
+						phone
+						socialMedia {
+							facebook
+							instagram
+							twitter
+							youTube
+						}
+						type
+						websiteUrl
+						yearFounded
+					}
+					characteristics {
+						aged
+						covering
+						flavors
+						milk
+						rennetType
+						standardsAndProcessing
+						style
+						texture
+					}
+					description
+					labelOrPhoto {
+						_id
+						caption
+						credit
+						description
+						filename
+						mimeType
+						path
+						sourceUrl
+						title
+						uploadStatus
+					}
+					name
+					pairings
+					photos {
+						photo {
+							_id
+							caption
+							credit
+							description
+							filename
+							mimeType
+							path
+							sourceUrl
+							title
+							uploadStatus
+						}
+					}
+					source
+					tastesLike {
+						_id
+						characteristics {
+							aged
+							covering
+							flavors
+							milk
+							rennetType
+							standardsAndProcessing
+							style
+							texture
+						}
+						description
+						name
+						pairings
+						source
+					}
+				}
 			}
 		}
 	}
