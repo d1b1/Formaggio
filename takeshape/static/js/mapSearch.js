de9be39ab387a34e3a5a19055d716ddf157da47a
Stephan Smith <stephan@meenta.io>
2019-11-05 22:14:01 -0500

    Updated the map

--- takeshape/static/js/mapSearch.js
+++ takeshape/static/js/mapSearch.js
@@ -1,62 +1,96 @@
 
-$script(
-	'https://maps.googleapis.com/maps/api/js?v=weekly&key=AIzaSyCZV7qjwy_JL03EguX6JcmplaFYKzZkK28',
-	// 'https://maps.googleapis.com/maps/api/js?v=weekly&key=AIzaSyBawL8VbstJDdU5397SUX7pEt9DslAwWgQ',
-	function() {
-		// var search = instantsearch({
-		//	 searchClient: algoliasearch(
-		//		 'latency',
-		//		 '6be0576ff61c053d5f9a3225e2a90f76'
-		//	 ),
-		//	 indexName: 'airbnb',
-		//	 routing: true,
-		// });
+// var apts = instantsearch({
+//   appId: 'OBBTFVLBPT',
+//   apiKey: 'b2bb1464328d945084725777e82f0536',
+//   indexName: 'cheese',
+// });
 
-		var search = instantsearch({
-			searchClient: algoliasearch('OBBTFVLBPT', 'b2bb1464328d945084725777e82f0536'),
-			indexName: 'cheese',
-			routing: true
-		});
+var apts = instantsearch({
+  appId: 'latency',
+  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+  indexName: 'airbnb',
+});
 
-		search.addWidgets([
+var aptsHits = instantsearch.widgets.hits({
+  container: document.querySelector('#hits'),
+  hitsPerPage: 3,
+  templates: {
+    item: '{{name}}'
+  }
+});
 
-			instantsearch.widgets.configure({
-				hitsPerPage: 1000,
-				aroundLatLngViaIP: true
-			}),
-
-			instantsearch.widgets.poweredBy({
-				container: '#powered-by'
-			}),
-
-			instantsearch.widgets.searchBox({
-				container: '#search-query',
-				cssClasses: {
-					input: 'input is-large'
-				},
-				placeholder: 'Search for cheese info...'
-			}),
-
-			instantsearch.widgets.geoSearch({
-				container: '#map',
-				googleReference: window.google,
-				initialZoom: 5,
-				enableRefine: true,
-				enableClearMapRefinement: true,
-				enableRefineControl: true,
-				enableRefineOnMapMove: true,
-				initialPosition: {
-					lat: 42.338477,
-					lng: -71.073187
-				},
-			}),
-
-			// instantsearch.widgets.stats({
-			//	 container: '#stats'
-			// })
-
-		])
-
-		search.start();
+var searchBox = instantsearch.widgets.searchBox({
+  container: document.querySelector('#search-query'),
+  wrapInput: false,
+	cssClasses: {
+		input: 'input is-large'
 	}
-);
+});
+
+var customMapWidget = {
+  _autocompleteContainer: document.querySelector('#places'),
+  _mapContainer: document.querySelector('#map'),
+  _hitToMarker: function(hit) {
+
+		if (!hit._geoloc) return null;
+
+    return new google.maps.Marker({
+      position: {
+					lat: hit._geoloc.lat,
+					lng: hit._geoloc.lng
+			},
+      map: this._map,
+      title: hit.name
+    });
+  },
+  _handlePlaceChange: function(place) {
+  	// https://developers.google.com/maps/documentation/javascript/reference#Autocomplete
+  	var place = this._autocomplete.getPlace();
+
+		if (place.geometry === undefined) {
+      // user did not select any place, see https://developers.google.com/maps/documentation/javascript/reference#Autocomplete
+      // events paragraph
+			if (place.name === '') {
+        // input was cleared
+        this._helper
+          .setQueryParameter('aroundLatLng')
+          .search();
+      }
+      return;
+    }
+
+    // see https://developers.google.com/maps/documentation/javascript/reference#PlaceResult
+    var latlng = place.geometry.location.toUrlValue();
+
+    // https://www.algolia.com/doc/guides/geo-search/geo-search-overview/#filter-and-sort-around-a-location
+    this._helper
+      .setQueryParameter('aroundLatLng', latlng)
+      .search();
+  },
+  init: function(params) {
+  	this._helper = params.helper;
+    this._autocomplete = new google.maps.places.Autocomplete(this._autocompleteContainer);
+    this._autocomplete.addListener('place_changed', this._handlePlaceChange.bind(this));
+    this._map = new google.maps.Map(this._mapContainer, {zoom: 1, center: new google.maps.LatLng(0, 0)});
+  },
+  render: function(params) {
+    var markers = params.results.hits.map(this._hitToMarker.bind(this));
+    var bounds = new google.maps.LatLngBounds();
+    markers.forEach(function(marker) {
+			// console.log('ddd', marker);
+			try {
+				bounds.extend(marker.getPosition());
+			} catch(err) {
+
+			}
+      //
+    });
+    this._map.fitBounds(bounds);
+  }
+};
+
+apts.addWidget(searchBox);
+apts.addWidget(aptsHits);
+apts.addWidget(customMapWidget);
+
+apts.start();
---
+++ takeshape/static/js/mapSearch2.js
@@ -0,0 +1,149 @@
+/* global algoliasearch instantsearch */
+
+function injectScript(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = src;
+    script.addEventListener('load', resolve);
+    script.addEventListener('error', () => reject('Error loading script.'));
+    script.addEventListener('abort', () => reject('Script loading aborted.'));
+
+    document.head.appendChild(script);
+  });
+}
+
+injectScript(
+  'https://maps.googleapis.com/maps/api/js?key=AIzaSyAKBlmyMd5Bkdv3SrZrQtyrO77m6Wt3Z34'
+).then(function() {
+
+    const searchClient = algoliasearch(
+      'OBBTFVLBPT',
+      'b2bb1464328d945084725777e82f0536'
+    );
+
+    const search = instantsearch({
+      indexName: 'cheese',
+      searchClient,
+    });
+
+		search.addWidget(
+			instantsearch.widgets.configure({
+				hitsPerPage: 10,
+			})
+		)
+
+    search.addWidget(
+      instantsearch.widgets.searchBox({
+        container: '#search-query',
+				showReset: false,
+				showSubmit: false,
+				showLoadingIndicator: true,
+				placeholder: 'Enter a cheese name...',
+				cssClasses: {
+					input: 'input is-large'
+				},
+      })
+    );
+
+    search.addWidget(
+      instantsearch.widgets.geoSearch({
+        container: '#map',
+        googleReference: window.google,
+				enableRefineControl: true,
+    		enableRefineOnMapMove: false,
+				mapOptions: {
+			    streetViewControl: true,
+			  },
+				enableRefine: true,
+				builtInMarker: {
+			    createOptions(item) {
+			      return {
+			        title: item.name,
+			      };
+			    },
+			    events: {
+			      click({ event, item, marker, map }) {
+			        console.log(item);
+
+							var infowindow = new google.maps.InfoWindow({
+								content: `
+									<div style="width: 300px;">
+										<figure class="image is-48x48" style="padding: 5px; border:1px solid #DFDFDF; float: left; margin-right: 10px;">
+											<img src="${item.photoUrl}">
+										</figure>
+
+										<b style="font-size: 18px;">
+											${item.name}
+										</b>
+										<p>
+											${item.description || ''}
+										</p>
+										<p>
+											<b>Style:</b> ${item.characteristics.style}
+										</p>
+										<p>
+											<b>Maker:</b> ${item.agents[0].name}
+										</p>
+										<br>
+										<b>Milk:</b> ${item.characteristics.milk}<br>
+										<b>Texture:</b> ${item.characteristics.texture}<br>
+										<b>Rennet:</b> ${item.characteristics.rennetType}<br>
+										<b>Rind:</b> ${item.characteristics.covering}<br>
+									</div>
+								`
+							});
+
+							var marker = new google.maps.Marker({
+								position: { lat: item._geoloc.lat, lng: item._geoloc.lng },
+								map: map,
+								title: 'Uluru (Ayers Rock)'
+							});
+
+							marker.addListener('click', function() {
+								infowindow.open(map, marker);
+							});
+			      },
+			    },
+			  },
+      })
+    );
+
+		search.addWidget(
+			instantsearch.widgets.hits({
+				container: '#hits',
+				templates: {
+					item: `
+						<figure class="image is-48x48" style="padding: 5px; border:1px solid #DFDFDF; float: left; margin-right: 10px;">
+							<img src="{{ photoUrl}}">
+						</figure>
+						<h4>
+							{{{ _highlightResult.name.value }}}
+						</h4>
+						Milk: {{ characteristics.milk }},
+						Texture: {{ characteristics.texture }}
+					`,
+					empty: `
+						<div id="no-results-message">
+							<p>
+								We didn't find any results for the search <em>"{{query}}"</em>.
+							</p>
+							<a href="." class='clear-all'>Clear search</a>
+						</div>
+					`
+				},
+				cssClasses: {
+					list: 'hit-list',
+					items: 'hit-item'
+				},
+				transformData: {
+					item(item) {
+						return item;
+					},
+				},
+			})
+		);
+
+    search.start();
+  }
+);
